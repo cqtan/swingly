8704298eaf3b0473f93bb103a53108c9b15ab98b
ctan <s0547139@htw-berlin.de>
2019-08-15 18:57:33 +0200

    Fix 2 of renaming

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/_app/App';
+import App from './components/app/App';
 import * as serviceWorker from './serviceWorker';
 import Root from './Root';
 
