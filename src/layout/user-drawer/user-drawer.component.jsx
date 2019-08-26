d2785a6498028c1783dc5386bc935d5b1064131e
ctan <s0547139@htw-berlin.de>
2019-08-26 12:31:44 +0200

    Hide component on link

--- src/layout/user-drawer/user-drawer.component.jsx
+++ src/layout/user-drawer/user-drawer.component.jsx
@@ -4,24 +4,36 @@ import {
   DrawerButton,
 } from './user-drawer.styles'
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { compose } from 'redux';
 import { signOut } from '../../redux/user/user.actions';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Backdrop from '../../ui/backdrop/backdrop.component';
+import { withRouter } from 'react-router-dom';
 
 const UserDrawer = (props) => {
-  const { isOpen, setOpen, signOut } = props;
+  const { isOpen, setOpen, signOut, history } = props;
+
+  const handleLink = linkPath => {
+    setOpen(false);
+    history.push(linkPath);
+  }
+
+  const handleSignOut = () => {
+    setOpen(false);
+    signOut();
+    history.push('/');
+  }
 
   return (
     <>
       <Backdrop onClick={() => setOpen(false)} isOpen={isOpen}/>
       <UserDrawerContainer isOpen={isOpen}>
-        <DrawerButton flat>
+        <DrawerButton flat onClick={() => handleLink('/profile')}>
           <FontAwesomeIcon icon='user-circle' />
-          <Link to='/profile'>Profile</Link>
+          Profile
         </DrawerButton>
         <hr />
-        <DrawerButton flat onClick={() => signOut()}>
+        <DrawerButton flat onClick={handleSignOut}>
           <FontAwesomeIcon icon='sign-out-alt' />
           Signout
         </DrawerButton>
@@ -34,4 +46,7 @@ const mapDispatchToProps = {
   signOut
 }
 
-export default connect(null, mapDispatchToProps)(UserDrawer);
+export default compose(
+  withRouter,
+  connect(null, mapDispatchToProps)
+)(UserDrawer);
