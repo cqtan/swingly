ef0419705b4a0803d165641d9ac369bc0d95a969
ctan <s0547139@htw-berlin.de>
2019-08-28 14:43:35 +0200

    Update app test

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -3,15 +3,24 @@ import { App } from './App';
 import { shallow } from 'enzyme';
 
 describe('App component', () => {
+  console.log('env: ', process.env.NODE_ENV);
+  
   let wrapper;
   const mockThemeMode = { darkMode: true };
   const mockProps = {
-    themeMode: mockThemeMode
+    themeMode: mockThemeMode,
+    snackbar: {
+      type: '',
+      text: '',
+      isOpen: false
+    }
   };
 
   wrapper = shallow(<App {...mockProps}/>);
 
   it('should render', () => {
+    console.log('env: ', process.env.NODE_ENV);
+
     expect(wrapper).toMatchSnapshot();
   });
 });
