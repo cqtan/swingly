372816b25d2ecd7f17f2e214fedbc0969cc07d7e
ctan <s0547139@htw-berlin.de>
2019-11-03 21:42:20 +0100

    Add updated event data for november

--- services/puppeteer/puppeteer.routes.js
+++ services/puppeteer/puppeteer.routes.js
@@ -1,168 +1,171 @@
-const express = require('express');
-const router = express.Router();
-const { firestore, getEnvironment, writeArrayToJsonObj } = require('../firebase/firebase-db.utils');
-const { scrapeEvents, formatEvents } = require('./puppeteer.utils');
-const fs = require('fs');
-const path = require('path');
+const express = require("express")
+const router = express.Router()
+const {
+  firestore,
+  getEnvironment,
+  writeArrayToJsonObj
+} = require("../firebase/firebase-db.utils")
+const { scrapeEvents, formatEvents } = require("./puppeteer.utils")
+const fs = require("fs")
+const path = require("path")
 
+router.get("/simple", (req, res) => {
+  const text = `simplest test works!`
+  res.send(text)
+})
 
-router.get('/simple', (req, res) => {
-  const text = `simplest test works!`;
-  res.send(text);
-});
+router.post("/test/:text", (req, res) => {
+  let text = req.params.text
+  text = `Your ${text} has been modified!`
+  res.send(text)
+})
 
-router.post('/test/:text', (req, res) => {
-  let text = req.params.text;
-  text = `Your ${text} has been modified!`;
-  res.send(text);
-});
+router.post("/store/:text", async (req, res) => {
+  let text = req.params.text
 
-router.post('/store/:text', async (req, res) => {
-  let text = req.params.text;
-  
-  text = `Your ${text} has been stored!`;
+  text = `Your ${text} has been stored!`
 
   try {
-    const newDocRef = firestore.collection(`${getEnvironment()}/data/text`).doc();
+    const newDocRef = firestore
+      .collection(`${getEnvironment()}/data/text`)
+      .doc()
 
     let dummyData = {
       id: newDocRef.id,
       text: text
-    };
+    }
 
     await newDocRef.set({
       dummyData
-    });
-    console.log("/store/:text SUCCESS!");
-    res.send(text);
-
+    })
+    console.log("/store/:text SUCCESS!")
+    res.send(text)
   } catch (error) {
-    console.log(error);
-    res.send(error);
+    console.log(error)
+    res.send(error)
   }
+})
 
-});
-
-router.post('/sample/:amount', async (req, res) => {
-  const amount = req.params.amount;
+router.post("/sample/:amount", async (req, res) => {
+  const amount = req.params.amount
   if (amount < 1) {
-    res.status(500).send('Please enter a number larger than 0');
+    res.status(500).send("Please enter a number larger than 0")
   }
 
   try {
-    let events = [];
-    let counter = 0;
+    let events = []
+    let counter = 0
 
     while (events.length < 2 && counter <= 5) {
-      counter++;
-      console.log(`Attempt(s) (${counter}) at scraping`);
-      events = await scrapeEvents();
+      counter++
+      console.log(`Attempt(s) (${counter}) at scraping`)
+      events = await scrapeEvents()
     }
 
     if (events.length < 1) {
-      res.status(404).send('Too many attempts...try again later!');
+      res.status(404).send("Too many attempts...try again later!")
     }
 
-    events = events.slice(0, amount);
-    events = formatEvents(events);
-    const batch = firestore.batch();
+    events = events.slice(0, amount)
+    events = formatEvents(events)
+    const batch = firestore.batch()
 
     for (event of events) {
-      const newDocRef = firestore.collection(`${getEnvironment()}/data/events`).doc();
+      const newDocRef = firestore
+        .collection(`${getEnvironment()}/data/events`)
+        .doc()
       batch.set(newDocRef, {
         ...event,
         id: newDocRef.id
-      });
+      })
     }
 
-    res.status(200).send(`Events ${events.length} have been persisted!`);
-    await batch.commit();
-    
+    res.status(200).send(`Events ${events.length} have been persisted!`)
+    await batch.commit()
   } catch (error) {
-    res.status(404).send('Events failed to persist! ', error);
+    res.status(404).send("Events failed to persist! ", error)
   }
-});
+})
 
-
-router.post('/refresh', async (req, res) => {
+router.post("/refresh", async (req, res) => {
   try {
-    let events = [];
-    let counter = 0;
-    let batch = firestore.batch();
-    const env = getEnvironment();
-    // const env = 'production';
+    let events = []
+    let counter = 0
+    let batch = firestore.batch()
+    // const env = getEnvironment();
+    const env = "production"
 
     // Delete All Events
     try {
-      const eventsSnap = await firestore.collection(`${env}/data/events`).get();
+      const eventsSnap = await firestore.collection(`${env}/data/events`).get()
       for (let eventSnap of eventsSnap.docs) {
-        batch.delete(eventSnap.ref);
+        batch.delete(eventSnap.ref)
       }
-      await batch.commit();
+      await batch.commit()
     } catch (err) {
-      console.log('Delete err: ', err);
+      console.log("Delete err: ", err)
     }
 
     // Scrape New Events
     while (events.length < 2 && counter <= 5) {
-      counter++;
-      console.log(`Attempt(s) (${counter}) at scraping`);
-      events = await scrapeEvents();
+      counter++
+      console.log(`Attempt(s) (${counter}) at scraping`)
+      events = await scrapeEvents()
     }
 
     if (events.length < 1) {
-      res.status(404).send('Too many attempts...try again later!');
+      res.status(404).send("Too many attempts...try again later!")
     }
 
     // Persist Formated Events
-    events = formatEvents(events);
-    const localMockEvents = [];
-    batch = firestore.batch();
+    events = formatEvents(events)
+    const localMockEvents = []
+    batch = firestore.batch()
 
     for (event of events) {
-      const newDocRef = firestore.collection(`${env}/data/events`).doc();
+      const newDocRef = firestore.collection(`${env}/data/events`).doc()
       const newEvent = {
         ...event,
         id: newDocRef.id
-      };
+      }
 
       batch.set(newDocRef, {
         ...newEvent
-      });
-      
-      localMockEvents.push(newEvent);
+      })
+
+      localMockEvents.push(newEvent)
     }
-    await batch.commit();
-    
-    if (getEnvironment() !== 'production') {
-      writeArrayToJsonObj(localMockEvents, '../puppeteer/EVENTS_DATA.json');    
+    await batch.commit()
+
+    if (getEnvironment() !== "production") {
+      writeArrayToJsonObj(localMockEvents, "../puppeteer/EVENTS_DATA.json")
     }
 
-    res.status(200).send(`Events ${events.length} have been persisted!`);
+    res.status(200).send(`Events ${events.length} have been persisted!`)
   } catch (error) {
-    res.status(404).send('Events failed to persist! ', error);
+    res.status(404).send("Events failed to persist! ", error)
   }
-});
+})
 
-router.get('/mockevents', async (req, res) => {
-  if (getEnvironment() === 'development' || getEnvironment() === 'test') {
-    const filePath = path.join(__dirname, 'EVENTS_DATA.json');
+router.get("/mockevents", async (req, res) => {
+  if (getEnvironment() === "development" || getEnvironment() === "test") {
+    const filePath = path.join(__dirname, "EVENTS_DATA.json")
 
     try {
-      fs.readFile(filePath, 'utf8', (error, data) => {
+      fs.readFile(filePath, "utf8", (error, data) => {
         if (!error) {
-          const parsedData = JSON.parse(data);          
-          res.status(200).send(parsedData);
+          const parsedData = JSON.parse(data)
+          res.status(200).send(parsedData)
         } else {
-          throw new Error(`Read file error for file ${filePath}: ` + error);
+          throw new Error(`Read file error for file ${filePath}: ` + error)
         }
-      });
+      })
     } catch (err) {
-      res.status(500).send(err.message);
+      res.status(500).send(err.message)
     }
   } else {
-    res.status(401).send('Not meant for you!');
+    res.status(401).send("Not meant for you!")
   }
-});
+})
 
-module.exports = router;
+module.exports = router
--- services/puppeteer/puppeteer.utils.js
+++ services/puppeteer/puppeteer.utils.js
@@ -1,205 +1,224 @@
-const puppeteer = require('puppeteer');
-const faker = require('faker');
-const { uniqWith, isEqual } = require('lodash');
-const fs = require('fs');
+const puppeteer = require("puppeteer")
+const faker = require("faker")
+const { uniqWith, isEqual } = require("lodash")
+const fs = require("fs")
 
 async function startBrowser() {
   const browser = await puppeteer.launch({
-    args: ['--lang=en-US']
-  });
-  const page = await browser.newPage();
-  const userAgent = await faker.internet.userAgent();
-  await page.setUserAgent(userAgent);
-  return {browser, page};
+    args: ["--lang=en-US"]
+  })
+  const page = await browser.newPage()
+  const userAgent = await faker.internet.userAgent()
+  await page.setUserAgent(userAgent)
+  return { browser, page }
 }
 
 async function closeBrowser(browser) {
-  return browser.close();
+  return browser.close()
 }
 
 function translate(month) {
-  switch(month) {
-    case 'Januar':
-      return 'January';
-    case 'Februar':
-      return 'February';
-    case 'März':
-      return 'March';
-    case 'April':
-      return 'April';
-    case 'Mai':
-      return 'May';
-    case 'Juni':
-      return 'June';
-    case 'Juli':
-      return 'July';
-    case 'August':
-      return 'August';
-    case 'September':
-      return 'September';
-    case 'Oktober':
-      return 'October';
-    case 'November':
-      return 'November';
-    case 'Dezember':
-      return 'December';
+  switch (month) {
+    case "Januar":
+      return "January"
+    case "Februar":
+      return "February"
+    case "März":
+      return "March"
+    case "April":
+      return "April"
+    case "Mai":
+      return "May"
+    case "Juni":
+      return "June"
+    case "Juli":
+      return "July"
+    case "August":
+      return "August"
+    case "September":
+      return "September"
+    case "Oktober":
+      return "October"
+    case "November":
+      return "November"
+    case "Dezember":
+      return "December"
     default:
-      return month;
+      return month
   }
 }
 
 // TODO: Make sure to remove sub-string once matched!
 function formatTime(timeString) {
-  let parts = timeString.split('–');
-  const regTime = /\d{2}\:\d{2}/;
-  const startTime = parts[0].match(regTime);
-  parts[0] = parts[0].replace(regTime, "");
-  const endTime = parts[1].match(regTime);
-  parts[1] = parts[1].replace(regTime, "");
+  let parts = timeString.split("–")
+  const regTime = /\d{2}\:\d{2}/
+  const startTime = parts[0].match(regTime)
+  parts[0] = parts[0].replace(regTime, "")
+  const endTime = parts[1].match(regTime)
+  parts[1] = parts[1].replace(regTime, "")
 
-  const regMonth = /[a-zA-Z.]{3,9}/;
-  let startMonth = parts[0].match(regMonth);
-  startMonth = translate(startMonth[0]);
-  parts[0] = parts[0].replace(regMonth, "");
-  let endMonth = parts[1].match(regMonth);
-  endMonth = endMonth === null ? startMonth : translate(endMonth[0]);
-  parts[1] = parts[1].replace(regMonth, "");
+  const regMonth = /[a-zA-Z.]{3,9}/
+  let startMonth = parts[0].match(regMonth)
+  startMonth = translate(startMonth[0])
+  parts[0] = parts[0].replace(regMonth, "")
+  let endMonth = parts[1].match(regMonth)
+  endMonth = endMonth === null ? startMonth : translate(endMonth[0])
+  parts[1] = parts[1].replace(regMonth, "")
 
-  const regDay = /\d{1,2}/;
-  const startDay = parts[0].match(regDay);
-  let endDay = parts[1].match(regDay);
-  endDay = endDay === null ? startDay[0] : endDay[0];
+  const regDay = /\d{1,2}/
+  const startDay = parts[0].match(regDay)
+  let endDay = parts[1].match(regDay)
+  endDay = endDay === null ? startDay[0] : endDay[0]
 
-  const year = new Date().getFullYear();
+  const year = new Date().getFullYear()
 
-  const startDate = new Date(`${startMonth} ${startDay}, ${year} ${startTime}:00`);
-  const endDate = new Date(`${endMonth} ${endDay}, ${year} ${endTime}:00`);
-  return [startDate, endDate];
+  const startDate = new Date(
+    `${startMonth} ${startDay}, ${year} ${startTime}:00`
+  )
+  const endDate = new Date(`${endMonth} ${endDay}, ${year} ${endTime}:00`)
+  return [startDate, endDate]
 }
 
 function formatDescription(description) {
-  let cleaned = description.replace(/<wbr>/g, '');
-  cleaned = cleaned.replace(/&amp;/g, '&');
+  let cleaned = description.replace(/<wbr>/g, "")
+  cleaned = cleaned.replace(/&amp;/g, "&")
 
-  return cleaned;
+  return cleaned
 }
 
 const getDetails = async detailItems => {
-  let when = null;
-  let startDate = null;
-  let endDate = null;
-  let location = null;
-  let mapLink = null;
-  let description = null;
-  
+  let when = null
+  let startDate = null
+  let endDate = null
+  let location = null
+  let mapLink = null
+  let description = null
+
   for (let detailItem of detailItems) {
-    if (await detailItem.$('div > span.event-when') !== null) {
-      when = await detailItem.$eval('div > span.event-when', el => el.innerText);
-      [ startDate, endDate ] = formatTime(when);
+    if ((await detailItem.$("div > span.event-when")) !== null) {
+      when = await detailItem.$eval("div > span.event-when", el => el.innerText)
+      ;[startDate, endDate] = formatTime(when)
     }
 
-    if (await detailItem.$('div > span.event-where') !== null)
-      location = await detailItem.$eval('div > span.event-where', el => el.innerText);
+    if ((await detailItem.$("div > span.event-where")) !== null)
+      location = await detailItem.$eval(
+        "div > span.event-where",
+        el => el.innerText
+      )
 
-    if (await detailItem.$('div > span.event-where > a') !== null)
-      mapLink = await detailItem.$eval('div > span.event-where > a', el => el.getAttribute('href'));
+    if ((await detailItem.$("div > span.event-where > a")) !== null)
+      mapLink = await detailItem.$eval("div > span.event-where > a", el =>
+        el.getAttribute("href")
+      )
 
-    if (await detailItem.$('div > span.event-description') !== null) {
-      description = await detailItem.$eval('div > span.event-description', el => el.innerText);
-      description = formatDescription(description);
+    if ((await detailItem.$("div > span.event-description")) !== null) {
+      description = await detailItem.$eval(
+        "div > span.event-description",
+        el => el.innerText
+      )
+      description = formatDescription(description)
     }
   }
 
   return {
     start: startDate,
     end: endDate,
-    location: location === null ? 'none' : location, 
-    mapLink: mapLink === null ? 'none' : mapLink, 
-    description: description === null ? 'none': description
+    location: location === null ? "none" : location,
+    mapLink: mapLink === null ? "none" : mapLink,
+    description: description === null ? "none" : description
   }
 }
 
 const scrapeEvents = async () => {
-  const {browser, page} = await startBrowser();
-  await page.goto('http://swinginberlin.de/', {
-    waitUntil: 'networkidle0'
-  });
+  const { browser, page } = await startBrowser()
+  await page.goto("http://swinginberlin.de/", {
+    waitUntil: "networkidle0"
+  })
 
-  let nestedFrame = null;
+  let nestedFrame = null
 
   for (const frame of page.mainFrame().childFrames()) {
     if (frame._name === "Content") {
-      nestedFrame = frame;
+      nestedFrame = frame
     }
   }
 
   // let linkNames = [];
-  let data = [];
+  let data = []
 
   if (nestedFrame !== null) {
-    const monthRows = await nestedFrame.$$('#mvEventContainer2 > div');
+    const monthRows = await nestedFrame.$$("#mvEventContainer2 > div")
     for (let monthRow of monthRows) {
-      const eventRows = await monthRow.$$('table.st-grid > tbody > tr');
+      const eventRows = await monthRow.$$("table.st-grid > tbody > tr")
       for (let eventRow of eventRows) {
-        if (await eventRow.$('td.st-c') !== null) {
-          const eventsWrapper = await eventRow.$$('td.st-c');
+        if ((await eventRow.$("td.st-c")) !== null) {
+          const eventsWrapper = await eventRow.$$("td.st-c")
           for (let eventWrapper of eventsWrapper) {
-            const event = await eventWrapper.$('div.st-c-pos');
-            if (event !== null) await event.click();
+            const event = await eventWrapper.$("div.st-c-pos")
+            if (event !== null) await event.click()
 
-            await nestedFrame.waitForSelector('div[id="bubbleContent\:1"]');
+            await nestedFrame.waitForSelector('div[id="bubbleContent:1"]')
 
-            const detailItems = await nestedFrame.$$('div.details > div.detail-content > div');
-            const details = await getDetails(detailItems);
+            const detailItems = await nestedFrame.$$(
+              "div.details > div.detail-content > div"
+            )
+            const details = await getDetails(detailItems)
 
-            let title = null;
-            if (await nestedFrame.$('div[id="bubbleContent\:1"] > div.details > span.title') !== null) 
-              title = await nestedFrame.$eval('div[id="bubbleContent\:1"] > div.details > span.title', el => el.innerText );
-            
-            data.push({ title, ...details });
+            let title = null
+            if (
+              (await nestedFrame.$(
+                'div[id="bubbleContent:1"] > div.details > span.title'
+              )) !== null
+            )
+              title = await nestedFrame.$eval(
+                'div[id="bubbleContent:1"] > div.details > span.title',
+                el => el.innerText
+              )
 
-            await page.keyboard.press('Escape');
+            data.push({ title, ...details })
+
+            await page.keyboard.press("Escape")
           }
         }
       }
     }
   }
-  await closeBrowser(browser);
+  await closeBrowser(browser)
 
-  const uniqueEvents = uniqWith(data, isEqual);
-  console.log('Raw: ', data.length);
-  console.log('Unique: ', uniqueEvents.length);
-  
-  return uniqueEvents;
+  const uniqueEvents = uniqWith(data, isEqual)
+  console.log("Raw: ", data.length)
+  console.log("Unique: ", uniqueEvents.length)
+
+  return uniqueEvents
 }
 
 const formatEvents = events => {
-  let deleteIndex = null;
+  let deleteIndex = null
   let newEvents = events.map((event, idx) => {
     if (event.title === null) {
-      deleteIndex = idx;
+      deleteIndex = idx
     }
 
     // wrlw2iC2tsOrM02iLm6OyUXfIv33 prod user
     return {
       ...event,
-      id: '',
+      id: "",
       images: [],
       cancelled: false,
-      hosts: ['A8WnU1fmQnWQ3bjvI6PYro32Tjh1'],
-      type: 'social',
-      currency: 'euro',
+      hosts: ["wrlw2iC2tsOrM02iLm6OyUXfIv33"],
+      type: "social",
+      currency: "euro",
       courses: [],
       otherFees: [],
-      guests: {},
+      guests: {}
     }
-  });
-  
-  newEvents.splice(deleteIndex, 1);
-  console.log(`${newEvents.length} have been formated!`);
+  })
 
-  return newEvents;
+  newEvents.splice(deleteIndex, 1)
+  console.log(`${newEvents.length} have been formated!`)
+
+  return newEvents
 }
 
-exports.formatEvents = formatEvents; 
-exports.scrapeEvents = scrapeEvents;
+exports.formatEvents = formatEvents
+exports.scrapeEvents = scrapeEvents
